89c12e6c105138f17b8097276aa8fde3ad65b912
b4ibmjava35@iiht.tech <b4ibmjava35@iiht.tech>
2019-09-13 21:56:21 +0530

    jdbc-authentication

---
+++ demo-13-09-2019_boot-security-jdbc-authentication/src/main/java/com/example/demo1192019/Demo1192019Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1192019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1192019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1192019Application.class, args);
+	}
+
+}
