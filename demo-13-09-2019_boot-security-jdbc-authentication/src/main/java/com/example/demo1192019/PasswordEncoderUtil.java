fff3276649ec70e1f7e23525587a959456c21d22
b4ibmjava35@iiht.tech <b4ibmjava35@iiht.tech>
2019-09-24 17:17:52 +0530

    security

--- demo-13-09-2019_boot-security-jdbc-authentication/src/main/java/com/example/demo1192019/PasswordEncoderUtil.java
+++ demo-13-09-2019_boot-security-jdbc-authentication/src/main/java/com/example/demo1192019/PasswordEncoderUtil.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderUtil {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encoded = encoder.encode("test123");//bob's password
+        String encoded = encoder.encode("priya");//bob's password
         System.out.println(encoded);
         encoded = encoder.encode("test234");//sara's password
         System.out.println(encoded);
