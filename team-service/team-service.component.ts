f3516f0646ce9e0a53c05d9ee0bd6a7c386f4d0d
b4ibmjava35@iiht.tech <b4ibmjava35@iiht.tech>
2019-09-26 20:28:26 +0530

    UI

---
+++ team-service/team-service.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Team } from '../Team/Team';
+
+@Component({
+  selector: 'app-team-service',
+  templateUrl: './team-service.component.html',
+  styleUrls: ['./team-service.component.css']
+})
+export class TeamServiceComponent implements OnInit {
+
+  
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
---
+++ team-service/view-team/view-team.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { Team } from 'src/app/Team/Team';
+import { on } from 'cluster';
+
+@Component({
+  selector: 'app-view-team',
+  templateUrl: './view-team.component.html',
+  styleUrls: ['./view-team.component.css']
+})
+export class ViewTeamComponent implements OnInit {
+
+  users: Team[]
+  tDetails: teamDetails[]
+  delemail: String
+  updateemail: String
+
+  constructor() { }
+
+  ngOnInit() {
+    const _baseUrl = `http://localhost:8050`;
+    fetch(_baseUrl + `/getByTaskId/1`,{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      } 
+    })
+    .then(res => res.json())
+    .then(res => {
+      this.users = res;
+      this.ints();
+    })
+  }
+
+  ints(){
+    this.tDetails = [];
+    for (let i= 0; i< this.users.length; i++){
+      let tName = {
+        email : this.users[i].uemail
+      }
+      this.tDetails[i] = (tName);
+    }
+  }
+
+  delete(email){
+    this.delemail  = email
+    const _baseUrl = `http://localhost:8050`;
+    fetch(_baseUrl + `/deleteByemail/${this.delemail}`, {
+      method: "GET"
+    })
+    this.ngOnInit();
+  }
+
+  update(email){
+    this.updateemail = email
+    const _baseUrl = `http://localhost:8050`;
+    fetch(_baseUrl + `/getByemail/${this.updateemail}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(res => {
+      this.users = res;
+      console.log(this.users);
+    })
+  }
+
+}
+
+interface teamDetails{
+  email: String
+}
